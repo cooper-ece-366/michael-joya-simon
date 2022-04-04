6de5b80b4d0fdf712d180d8746ca1827e04d69b4
Joya Debi <joyaranidebi@gmail.com>
2022-04-04 15:43:53 -0400

    single study buddy column

--- lava-app/src/components/Friends.js
+++ lava-app/src/components/Friends.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Friends.css';
+import FriendList from './FriendList';
+import frienddata from "./frienddata";
+
 
 class Friends extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            frienddata,
+            //const [value, setValue]= useState(frienddata)
+        };
+    }
     render() {
         return (
             <div>
-                Hello test
+                <div className="friendcontainer">
+                    <h1 className="title">
+                        Study Buddies
+                    </h1>
+                    <div>
+                        {this.state.frienddata.map((item)=>{
+                            return(<FriendList {...item} key={item.id}/>)
+                        })}
+
+                    </div>
+
+                </div>
             </div>
         )
     }
 }
 
 export default Friends;
+
+
+//make open boolean in state
+//apply that to div title to change when clicked
+//make so div data shown only shjown when open boolen true
---
+++ lava-app/src/components/frienddata.js
@@ -0,0 +1,45 @@
+const frienddata = [
+    {
+        id: '1',
+        name: 'Mikey b',
+        interests: 'Prob & Stoch, full stack dev',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '2',
+        name: 'Bob Saget',
+        interests: 'full house, tv production',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '3',
+        name: 'Rihanna',
+        interests: 'makeup, not putting out album',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '4',
+        name: 'Simon',
+        interests: 'Valorant, streaming',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '5',
+        name: 'teo',
+        interests: 'money, all natural products',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '6',
+        name: 'ali',
+        interests: 'anime, coding',
+        img: 'https://robohash.org',
+    },
+    {
+        id: '7',
+        name: 'DW',
+        interests: 'Being annoying',
+        img: 'https://robohash.org',
+    },
+];
+export default frienddata;
