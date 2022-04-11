aea14519057d345d5b0441069dc154340e6b95ad
Joya Debi <joyaranidebi@gmail.com>
2022-04-11 11:39:29 -0400

    Requests/sent toggle works, for later: change format to table to configure

--- lava-app/src/components/Friends.js
+++ lava-app/src/components/Friends.js
@@ -16,11 +16,17 @@ class Friends extends Component {
         this.state = {
             frienddata, requestdata, sentdata, isRequests: true,
         };
+        this.toggleIsRequests = this.toggleIsRequests.bind(this);
     }
+
+    toggleIsRequests() {
+        this.setState({isRequests: !this.state.isRequests});
+    }
+
     render() {
         return (
-                <div class="row">
-                    <div class="column">
+                <div className="row">
+                    <div className="column">
                         <h1 className="Study Buddies">
                             Study Buddies
                         </h1>
@@ -31,29 +37,23 @@ class Friends extends Component {
 
                         </div>
                     </div>
-                    {this.state.isRequests ?
-                    <div class="column">
-                        <h1 class= "Requests">
-                            Requests
-                        </h1>
+                    <div className="column">
+                        <div className="dropdown">
+                            <button onClick={() => {this.setState({isRequests:true})}} className={`dropbtn ${this.state.isRequests ? "active" : ""}`}>Requests</button>
+                            <button onClick={() => {this.setState({isRequests: false})}} className={`dropbtn ${this.state.isRequests ? "" : "active"}`}>Sent</button>
+                        </div>
+                        {this.state.isRequests ?
                         <div>
                             {this.state.requestdata.map((item   )=>{
                                 return(<RequestList {...item} key={item.id}/>)
                             })}
-                        </div>
-
-                    </div>:
-                    <div className="column">
-                        <h1 className="Sent">
-                            Sent
-                        </h1>
-                        <div>
-                            {this.state.sentdata.map((item) => {
-                                return (<SentList {...item} key={item.id}/>)
-                            })}
-                        </div>
-
-                    </div>}
+                        </div>:
+                            <div>
+                                {this.state.sentdata.map((item) => {
+                                    return (<SentList {...item} key={item.id}/>)
+                                })}
+                            </div>}
+                    </div>
                 </div>
         )
     }
