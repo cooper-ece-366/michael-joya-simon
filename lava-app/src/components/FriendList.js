705e65dabea5c7258ed41e5366c1b5bacdfe0e24
Simon Yoon <simon.yoon@cooper.edu>
2022-04-25 17:09:20 -0400

    remove friend button

--- lava-app/src/components/FriendList.js
+++ lava-app/src/components/FriendList.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { API_BASE_URL } from '../constants';
 import './FriendList.css';
+import {postDelete} from '../util/APIUtils.js'
 
 const FriendList=({id, name, skillsList, imageUrl})=>{
+    
+    function friendRemove() {
+        console.log("test");
+        postDelete(id);
+        window.location.reload();
+    }
+    
     return(
         <div>
             <section className="friend">
@@ -12,6 +21,7 @@ const FriendList=({id, name, skillsList, imageUrl})=>{
                     <h2 className="name">{name}</h2>
                     <p>{skillsList}</p>
                 </div>
+                <button onClick = {friendRemove} className = "remove-button">Remove</button>
             </section>
         </div>
     )
