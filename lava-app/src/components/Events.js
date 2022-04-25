4d92c84a432c9c5af43be1376087329765a9a1c0
Joya Debi <joyaranidebi@gmail.com>
2022-04-25 01:51:50 -0400

    added private route to calendar page

---
+++ lava-app/src/components/Events.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './Events.css';
+
+class Events extends Component {
+render(){
+    return(
+        <div>test</div>
+    )
+}
+}
+
+export default Events;
+
+//make an api that
